cf9c56e56eb9674bc98cadfef3806edec25efec4
jonamix-ar <jonatanvilella@gmail.com>
2024-12-20 16:38:21 -0300

    Test register google

--- src/app/(auth)/login/modules/SocialLogin/RegisterLogin.tsx
+++ src/app/(auth)/login/modules/SocialLogin/RegisterLogin.tsx
@@ -1,11 +1,32 @@
-import { signIn } from 'next-auth/react'
+import { getSession, signIn } from 'next-auth/react'
 import GoogleIcon from '@/components/Icons/GoogleIcon'
+import { useRouter } from 'next/navigation'
 
 const SocialRegister = () => {
-  const handleGoogleSignIn = async () => {
-    signIn('google')
-  }
+  // const handleGoogleSignIn = async () => {
+  //   signIn('google', { callbackUrl: '/dashboard' })
+  // }
 
+  const router = useRouter()
+
+  const handleGoogleSignIn = async () => {
+    try {
+      const result = await signIn('google', {
+        callbackUrl: '/customer',
+        redirect: false
+      });
+
+      if (result?.error) {
+        console.error('Error en el inicio de sesión con Google:', result.error);
+      } else if (result?.ok) {
+        // Redirigir al usuario a la página de cliente
+        router.push('/customer');
+      }
+    } catch (error) {
+      console.error('Error al iniciar sesión con Google:', error);
+    }
+  }
+  
   return (
     <div className="mb-3 w-full">
       <button
