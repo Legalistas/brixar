86dd547e801c7875cf31d479d480c33d2caa6e68
fibonaku <galileo.laino@gmail.com>
2025-03-06 16:09:00 -0300

    Agregar Google Tag Manager y Google Analytics al layout principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { ToastContainer } from "react-toastify";
 import { Providers } from "./Providers";
 import { Inter, Ubuntu } from 'next/font/google'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
 
-// If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ['latin'] })
 const ubuntu = Ubuntu({
   subsets: ['latin'],
@@ -57,10 +57,35 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="apple-mobile-web-app-title" content="Brix.ar" />
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l;'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5ZMJCRMC');
+          `}
+        </Script>
+        {/* Google Analytics */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-TKDYP32F6F" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-TKDYP32F6F');
+          `}
+        </Script>
       </head>
       <body
         className={`${inter.className} antialiased bg-[#F1F5F9]`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5ZMJCRMC"
+            height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+        </noscript>
         <Providers>
           {children}
           <GoogleAnalytics gaId={`${process.env.NEXT_PUBLIC_GID}`} />
