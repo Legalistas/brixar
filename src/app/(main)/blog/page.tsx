0fd92b62abb649d5b7a3dfb693095c6ddc0176e9
fibonaku <galileo.laino@gmail.com>
2025-04-08 15:46:25 -0300

    Actualizar título de la página del blog y mejorar la estructura del componente NavLinks

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -1,13 +1,13 @@
-import { getPosts } from "@/utils/wp";
-import PostCard from "@/components/PostCard";
+import { getPosts } from '@/utils/wp'
+import PostCard from '@/components/PostCard'
 
 export default async function BlogPage() {
-  const posts = await getPosts();
+  const posts = await getPosts()
 
   return (
     <div className="container mx-auto py-12 px-4">
-      <h1 className="text-3xl font-bold mb-8">Nuestro Blog</h1>
-      
+      <h1 className="text-3xl font-bold mb-8">Actualidad en Real State</h1>
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {posts.map((post) => (
           <PostCard
@@ -20,12 +20,12 @@ export default async function BlogPage() {
           />
         ))}
       </div>
-      
+
       {posts.length === 0 && (
         <div className="text-center py-10">
           <p className="text-xl text-gray-600">No hay posts disponibles.</p>
         </div>
       )}
     </div>
-  );
+  )
 }
