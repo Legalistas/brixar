20e8369ad4a51adb5da6bfb98b48d4c627e01455
jonamix-ar <jonatanvilella@gmail.com>
2025-07-02 13:23:07 -0300

    Change Google ADS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout({
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5ZMJCRMC');
+          })(window,document,'script','dataLayer','GTM-W38VBG7D');
           `}
         </Script>
 
@@ -95,7 +95,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5ZMJCRMC"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W38VBG7D"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
