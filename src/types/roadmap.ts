7c58928493ab02ccb87c274f90e168b6b9d46149
Legalistas <legalistas.web@gmail.com>
2025-07-03 15:57:28 -0300

    roadmap funcional

--- src/types/roadmap.ts
+++ src/types/roadmap.ts
@@ -1,25 +1,52 @@
+// export interface Task {
+//   id: number
+//   description: string
+//   proyectId?: number
+//   duration: number
+//   dependentTask: number | null
+//   dependencyType: "FC" | "CC" | "none"
+//   dependencyDays: number
+//   start: string | null // ISO date string
+//   end: string | null // ISO date string
+//   plannedStart: string | null
+//   plannedEnd: string | null
+//   realStart: string | null
+//   realEnd: string | null
+//   responsible: string
+//   status: "Pendiente" | "En Progreso" | "Completado"
+//   realEndDate: string | null
+//   effectiveDays: number | null
+//   isDelayed: boolean
+//   delayDays: number
+//   isAhead: boolean
+//   aheadDays: number
+// }
+
 export interface Task {
   id: number
   description: string
-  proyectId?: number
   duration: number
   dependentTask: number | null
-  dependencyType: "FC" | "CC" | "none"
+  dependencyType: 'FC' | 'CC' | 'none'
   dependencyDays: number
-  start: string | null // ISO date string
-  end: string | null // ISO date string
-  plannedStart: string | null
-  plannedEnd: string | null
-  realStart: string | null
-  realEnd: string | null
+  start: Date | null
+  end: Date | null
+  plannedStart: Date | null
+  plannedEnd: Date | null
+  realStart: Date | null
+  realEnd: Date | null
+  realEndDate: Date | null
   responsible: string
-  status: "Pendiente" | "En Progreso" | "Completado"
-  realEndDate: string | null
+  status: 'Pendiente' | 'En Progreso' | 'Completado'
   effectiveDays: number | null
   isDelayed: boolean
   delayDays: number
   isAhead: boolean
   aheadDays: number
+  isDefault: boolean
+  isHidden: boolean 
+  order: number 
+  isCustom: boolean
 }
 
 export interface Roadmap {
